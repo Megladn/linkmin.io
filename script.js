8e0c6381dfc84dc23338e60d9c9e0f70addfec00
Megladn <161157854+Megladn@users.noreply.github.com>
2024-02-26 19:44:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ function shortenUrl() {
 
 }
 
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("mycopy");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
 reloadBtn.addEventListener('click', () => {
     location.reload();
 });
